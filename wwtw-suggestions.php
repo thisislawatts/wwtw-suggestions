388c12ba143f16e3fcddf505cf83f80e37593a20
Luke Watts <luke@thisis.la>
2014-06-15 09:36:21 +0100

    Scaffolded

---
+++ wwtw-suggestions.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Suggestions
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: Luke Watts
+Author URI: http://thisis.la
+Plugin URI: http://github.com/thisislawatts
+Text Domain: wwtw-suggestions
+Domain Path: /languages
+*/
+
